eb33a9a06ff5b3c02da884af9e1e1f911867cd7c
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2016-08-11 20:06:22 +0300

    Add javadoc

--- src/test/java/com/example/MultipleViaCustomRule.java
+++ src/test/java/com/example/MultipleViaCustomRule.java
@@ -17,8 +17,13 @@ public final class MultipleViaCustomRule {
 	@Test
 	@Repeat(10)
 	public void multiple() {
-		/*
+		/*-
 		 * Only repeats test runs until the 1st failure.
+		 *
+		 * Given the fact that:
+		 *  - only the last invocation status is recorded, and
+		 *  - the last (10th) invocation always fails,
+		 * this test will have 100% rather than 10% failure rate.
 		 */
 		if (++this.j == 10) {
 			fail("Failed during run #" + this.j);
--- src/test/java/com/example/MultipleViaSpring.java
+++ src/test/java/com/example/MultipleViaSpring.java
@@ -17,8 +17,13 @@ public final class MultipleViaSpring {
 	@Repeat(10)
 	@Test
 	public void multiple() {
-		/*
+		/*-
 		 * Spring only repeats test runs until the 1st failure.
+		 *
+		 * Given the fact that:
+		 *  - only the last invocation status is recorded, and
+		 *  - the last (10th) invocation always fails,
+		 * this test will have 100% rather than 10% failure rate.
 		 */
 		if (++this.j == 10) {
 			fail("Failed during run #" + this.j);
